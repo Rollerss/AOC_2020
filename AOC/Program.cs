d6983a1601ed35c37dd04433d21f16ce71bda05e
Rollerss <oneouthere@gmail.com>
2020-11-30 21:46:52 -0800

    Add project files.

---
+++ AOC/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Day01.Find2020x2();
+            Day01.Find2020x3();
+        }
+    }
+}
